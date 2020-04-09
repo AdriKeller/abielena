f542eb88ad3ee03e8ef92f0093979037a6736990
Elena Baruc-Westhäuser <Bibielena@ElenasMacBook3.fritz.box>
2020-04-09 10:46:56 +0200

    readlevel.py aktualisiert

--- readlevel.py
+++ readlevel.py
@@ -1,11 +1,17 @@
 import pygame
 import block
+
+x = 0
+y = 0
 def drawlevel(levelnumber):
 	level = open("Level/Level"+ str(levelnumber) + ".txt")
-	levelcounter = 0
+	levelcounter_x = 0
+	levelcounter_y = 0
 	for line in level:
-		for element in line:
-			if element == "1":
-				return fenster.blit(block.block1, (30+levelcounter, 30+levelcounter))
-			levelcounter = levelcounter + 1
+		for element in line: 
+			#if element == "1":
+			#	fenster.blit(block.block1, (x, y))
+			levelcounter_x = levelcounter_x + 1
+			levelcounter_y = int(levelcounter_x / 30)
+			print("x=" + str(levelcounter_x%30) + "y=" + str(levelcounter_y))
 drawlevel(1)
