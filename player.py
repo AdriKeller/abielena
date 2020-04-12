a019a200ca7afca776509ab840b90064302e246e
Elena Baruc-Westhäuser <Bibielena@ElenasMacBook3.fritz.box>
2020-04-12 10:59:17 +0200

    player.py aktualisiert

--- player.py
+++ player.py
@@ -5,7 +5,7 @@ import level
 class Player(object):
 	
 	
-	def __init__(self, x, y, width, height, bildsource, fenster):
+	def __init__(self, x, y, width, height, bildsource, fenster, name):
 		self.x = x
 		self.y = y
 		self.width = width #von hitbox
@@ -17,6 +17,7 @@ class Player(object):
 		self.springzahl = 5
 		self.fenster = fenster
 		self.bild = pygame.image.load(bildsource)
+		self.name = name
 	
 	def draw(self):
 		self.fenster.blit(self.bild, (self.x, self.y))
@@ -32,19 +33,28 @@ class Player(object):
 				self.springstate = True #ändern dass er nicht einmal durch den pygame loop durchgeht bevor er anfängt zu springen
 		else:
 			if self.springzahl >= -5: #solange es -5 nicht erreicht
-				self.springzahl = self.springzahl - 2 #counter springzahl
 				if self.springzahl > 0:
-					self.bew_y = - (self.springzahl**2)
+					neg = -1
 				else:
-					self.bew_y =  self.springzahl**2
+					neg = 1
+				self.springzahl = self.springzahl - 2 #counter springzahl
+				self.bew_y = neg * (self.springzahl**2)
+
 			else: #variablen resetten wenn der sprung fertig ist
 				self.springstate = False
 				self.springzahl = 5
-		if not level.collision(self):
+		if not level.collision(self, level):
 			self.y = self.y + self.bew_y #bewegung
 			self.x = self.x + self.bew_x
-			self.bew_x = 0
-
+		self.bew_x = 0
+		self.bew_y = 0
+	
+	
+		def grav(self, level): #gravitation: wenn er nirgendswo anstößt, fällt er weiter
+			self.bew_y = 10 #to do: parabelförmig mit springzahl
+			if not level.collision(self):
+				self.y = self.y + self.bew_y
+			self.bew_y = 0 
 
 
 	
