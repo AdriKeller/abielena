ca2585a4f4a1d5fb4e9d37d94317800b5fcab5a0
Elena Baruc-Westhäuser <Bibielena@ElenasMacBook3.fritz.box>
2020-04-18 17:34:07 +0200

    gelberblock.png, gelberblock_ghost.png und 6 weitere dateien aktualisiert...

--- ghostblock.py
+++ ghostblock.py
@@ -6,7 +6,7 @@ stellt einen durchgängigen Stein dar (Unterklasse von Block)
 """
 class Ghostblock(block.Block):
 	"""
-	erstellt einen neuen durchgängigen Stein und ruft dabei die init funktion von Block auf
+	erstellt einen neuen durchlässigen Stein und ruft dabei die init funktion von Block auf
 	:param fenster: gibt fenster-Surface weiter um darauf malen zu können
 	:param x: x-Position des Blocks (zwischen 0 und 29)
 	:param y: y-Position des Blocks (zwischen 0 und 19)
@@ -19,12 +19,58 @@ class Ghostblock(block.Block):
 	def __init__(self, fenster, x, y, bildsource):
 		super().__init__(fenster, x, y, bildsource)
 		
-
-		
 class Buttonghostblock(Ghostblock):
 	
 	"""
-	erstellt einen neuen durchgängigen Stein und ruft dabei die init Funktion von Ghostbecken auf
+	erstellt einen neuen durchlässigen Stein und ruft dabei die init Funktion von Ghostbecken auf
+	:param fenster: gibt fenster-Surface weiter um darauf malen zu können
+	:param x: x-Position des Blocks (zwischen 0 und 29)
+	:param y: y-Position des Blocks (zwischen 0 und 19)
+	:param bildsource: path zur Bilddatei des Blocks
+	:type fenster: pygame.display
+	:type x: int
+	:type y: int
+	:type bildsource: str
+	"""
+	def __init__(self, fenster, x, y, bildsource):
+		super().__init__(fenster, x, y, bildsource)
+		
+class Gelberghostblock(Buttonghostblock):
+	
+	"""
+	erstellt einen gelben durchlässigen Stein und ruft dabei die init Funktion von Buttonghostblock auf
+	:param fenster: gibt fenster-Surface weiter um darauf malen zu können
+	:param x: x-Position des Blocks (zwischen 0 und 29)
+	:param y: y-Position des Blocks (zwischen 0 und 19)
+	:param bildsource: path zur Bilddatei des Blocks
+	:type fenster: pygame.display
+	:type x: int
+	:type y: int
+	:type bildsource: str
+	"""
+	def __init__(self, fenster, x, y, bildsource):
+		super().__init__(fenster, x, y, bildsource)
+		
+class Lilaghostblock(Buttonghostblock):
+	
+	"""
+	erstellt einen lilanen durchlässigen Stein und ruft dabei die init Funktion von Buttonghostblock auf
+	:param fenster: gibt fenster-Surface weiter um darauf malen zu können
+	:param x: x-Position des Blocks (zwischen 0 und 29)
+	:param y: y-Position des Blocks (zwischen 0 und 19)
+	:param bildsource: path zur Bilddatei des Blocks
+	:type fenster: pygame.display
+	:type x: int
+	:type y: int
+	:type bildsource: str
+	"""
+	def __init__(self, fenster, x, y, bildsource):
+		super().__init__(fenster, x, y, bildsource)
+
+class Roterghostblock(Buttonghostblock):
+	
+	"""
+	erstellt einen roten durchlässigen Stein und ruft dabei die init Funktion von Buttonghostblock auf
 	:param fenster: gibt fenster-Surface weiter um darauf malen zu können
 	:param x: x-Position des Blocks (zwischen 0 und 29)
 	:param y: y-Position des Blocks (zwischen 0 und 19)
@@ -41,7 +87,39 @@ class Buttonghostblock(Ghostblock):
 class Playerghostblock(Ghostblock):
 	
 	"""
-	erstellt einen neuen durchgängigen Stein pro Player und ruft dabei die init Funktion von Ghostbecken auf
+	erstellt einen neuen durchlässigen Stein pro Player und ruft dabei die init Funktion von Ghostbecken auf
+	:param fenster: gibt fenster-Surface weiter um darauf malen zu können
+	:param x: x-Position des Blocks (zwischen 0 und 29)
+	:param y: y-Position des Blocks (zwischen 0 und 19)
+	:param bildsource: path zur Bilddatei des Blocks
+	:type fenster: pygame.display
+	:type x: int
+	:type y: int
+	:type bildsource: str
+	"""
+	def __init__(self, fenster, x, y, bildsource):
+		super().__init__(fenster, x, y, bildsource)
+		
+class P1ghostblock(Playerghostblock):
+	
+	"""
+	erstellt einen rosanen durchlässigen Stein und ruft dabei die init Funktion von Playerghostbecken auf
+	:param fenster: gibt fenster-Surface weiter um darauf malen zu können
+	:param x: x-Position des Blocks (zwischen 0 und 29)
+	:param y: y-Position des Blocks (zwischen 0 und 19)
+	:param bildsource: path zur Bilddatei des Blocks
+	:type fenster: pygame.display
+	:type x: int
+	:type y: int
+	:type bildsource: str
+	"""
+	def __init__(self, fenster, x, y, bildsource):
+		super().__init__(fenster, x, y, bildsource)
+		
+class P2ghostblock(Playerghostblock):
+	
+	"""
+	erstellt einen schwarzen durchlässigen Stein und ruft dabei die init Funktion von Playerghostbecken auf
 	:param fenster: gibt fenster-Surface weiter um darauf malen zu können
 	:param x: x-Position des Blocks (zwischen 0 und 29)
 	:param y: y-Position des Blocks (zwischen 0 und 19)
